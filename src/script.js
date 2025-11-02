e6adabb0da7e582a52a3a7427b8917a6dce7f4cb
unknown <ghulsan.savanaa@gmail.com>
2025-11-03 00:55:35 +0700

    update togle menu responsive

--- src/script.js
+++ src/script.js
@@ -1,6 +1,28 @@
 const menuBtn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
+const closeBtn = document.getElementById('close-btn');
 
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('hidden');
+    closeBtn.classList.toggle('hidden');
+    menuBtn.classList.toggle('hidden');
 });
+
+closeBtn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+    closeBtn.classList.toggle('hidden');
+    menuBtn.classList.toggle('hidden');
+});
+
+// menuBtn.addEventListener('click', () => {
+
+// });
+
+// closeBtn.addEventListener('click', () => {
+    
+//     menu.classList.toggle('hidden');
+// })
+
+// closeBtn.addEventListener('click', () => {
+//     closeBtn.classList.toggle('hidden');
+// });
